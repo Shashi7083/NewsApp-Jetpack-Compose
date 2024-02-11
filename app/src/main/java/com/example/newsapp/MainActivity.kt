5a7ead6532418b9c0f2596f7ad6733b27e9313d1
Shashi7083 <shashikr12047@gmail.com>
2024-02-11 19:24:19 +0530

    dagger hilt issue solved

---
+++ app/src/main/java/com/example/newsapp/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.example.newsapp
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.view.WindowCompat
+import androidx.lifecycle.lifecycleScope
+import com.example.newsapp.domain.usecases.AppEntryUseCases
+import com.example.newsapp.presentation.onboarding.OnBoardingScreen
+import com.example.newsapp.ui.theme.NewsAppTheme
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+
+    @Inject
+    lateinit var appEntryUseCases: AppEntryUseCases
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        //for statusbar
+        WindowCompat.setDecorFitsSystemWindows(window,false)
+        installSplashScreen()
+        lifecycleScope.launch {
+            appEntryUseCases.readAppEntry().collect{
+                Log.d("Test", it.toString())
+            }
+        }
+        setContent {
+            NewsAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                        OnBoardingScreen()
+                }
+            }
+        }
+    }
+}
+
---
+++ app/src/main/java/com/example/newsapp/presentation/onboarding/components/PageIndicator.kt
@@ -0,0 +1,40 @@
+package com.example.newsapp.presentation.onboarding.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.example.newsapp.presentation.Dimens.IndicatorSize
+
+@Composable
+fun PageIndicator(
+    modifier: Modifier = Modifier,
+    pageSize: Int,
+    selectedPage: Int,
+    selectedColor: Color = MaterialTheme.colorScheme.primary,
+    unselectedColor: Color = Color.LightGray
+) {
+    Row(
+        modifier=modifier,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        repeat(pageSize) { page ->
+            Box(
+                modifier = Modifier
+                    .size(IndicatorSize)
+                    .clip(CircleShape)
+                    .background(color = if (page == selectedPage) selectedColor else unselectedColor)
+            )
+        }
+    }
+}
